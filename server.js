fa33a69fcb0e632657259a42d4c70a0a45d091d7
Samarth Joshi <samarthjoshi1608@gmail.com>
2025-09-08 17:35:05 +0200

    m

--- server.js
+++ server.js
@@ -1,7 +1,8 @@
 
 // server.js
 const app = require("./app");
-const port = 8080;
+
+const port = process.env.PORT || 8080;
 
 app.listen(port, () => {
     console.log(`Server is live on port ${port}`);
